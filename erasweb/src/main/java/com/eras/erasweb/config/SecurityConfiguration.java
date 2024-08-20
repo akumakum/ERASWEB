f4892101bd9ae7f76f26b7c66c73f348b7078a16
akumakum <joymonta2k@gmail.com>
2024-08-20 11:46:15 +0800

    Added logout

--- erasweb/src/main/java/com/eras/erasweb/config/SecurityConfiguration.java
+++ erasweb/src/main/java/com/eras/erasweb/config/SecurityConfiguration.java
@@ -34,17 +34,20 @@ public class SecurityConfiguration {
                     registry.requestMatchers("/backup/**").permitAll();
                     registry.anyRequest().authenticated();
                 //    registry.anyRequest().permitAll();
-                    
-                    
-                    
-                    
-                    
                 })
                 .formLogin(httpSecurityFormLoginConfigurer -> {
                     httpSecurityFormLoginConfigurer.loginPage("/login")
                             .successHandler(authenticationSuccessHandler)
                             .permitAll();
                 })
+                .logout(logout ->
+                logout
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/login?logout")
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID")
+            )
+                
                 .build();
     }
 
@@ -66,10 +69,7 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
     
-//    @Bean
-//    WebSecurityCustomizer websecurityCustomizer() {
-//    	return (web)->web.ignoring().requestMatchers("/ajax/**");
-//    }
+
 }
 
 
@@ -77,72 +77,3 @@ public class SecurityConfiguration {
 
 
 
-//package com.eras.erasweb.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.authentication.AuthenticationProvider;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
-//
-//@Configuration
-//public class SecurityConfiguration {
-//	
-//	@Autowired
-//	UserDetailsService userDetailService;
-//	
-//	@Bean
-//	SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
-//		return httpSecurity
-//				.csrf(AbstractHttpConfigurer::disable)
-//				.authorizeHttpRequests(registry->{
-//			registry.requestMatchers("/patientrecords/**").hasRole("ADMIN");
-//			registry.requestMatchers("/maintenance/**").hasRole("ADMIN"); //temp until there is a new endpoint for patient
-//			registry.requestMatchers("/reports/**").hasRole("REPORTS");
-//		//	registry.anyRequest().permitAll();
-//			registry.anyRequest().authenticated() ;   //.permitAll();   //authenticated();
-//		})
-//				.formLogin(httpSecurityFormLoginConfigurer -> { 
-//					httpSecurityFormLoginConfigurer.loginPage("/login")
-//					.successHandler(new AuthenticationSuccessHandler())
-//					.permitAll();
-//					
-//				})
-//				
-//				
-//				//.formLogin().loginPage("/login")
-//
-//		.build();
-//	}
-//	
-//
-//	
-//	@Bean
-//	public UserDetailsService userDetailService() {
-//		return userDetailService;
-//	}
-//	
-//	
-//	
-//	@Bean
-//	AuthenticationProvider authenticationProvider() {
-//		DaoAuthenticationProvider provider =new DaoAuthenticationProvider();
-//		provider.setUserDetailsService(userDetailService);
-//		provider.setPasswordEncoder(passwordEncoder());
-//		return provider;
-//		
-//	}
-//	
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
-//}
--- erasweb/src/main/java/com/eras/erasweb/controller/SettingsController.java
+++ erasweb/src/main/java/com/eras/erasweb/controller/SettingsController.java
@@ -116,9 +116,10 @@ public class SettingsController {
 	public String erasAdminMain(HttpSession session, Model model) {
 		String username = (String) session.getAttribute("username");
 		Hospital hospital = (Hospital) session.getAttribute("hospital");
+		MainPageURL mainPageURL = (MainPageURL) session.getAttribute("mainPageURL");
 		model.addAttribute("username", username);
 		model.addAttribute("hospital", hospital);
-
+		model.addAttribute("mainPageURL", mainPageURL);
 		return "Main";
 	}
 
